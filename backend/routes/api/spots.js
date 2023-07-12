62419439dffa20747f56015e2d006c9f384a54da
JuvenalBurguillos <juvie3@gmail.com>
2023-07-12 01:25:23 -0400

    completed 4 spots route handlers

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
 
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
@@ -9,10 +10,134 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/current", requireAuth, async (req, res, next) => {
+
+      const userSpots = await Spot.findAll({
+            where: {
+                  ownerId: req.user.id
+            }
+      })
+
+      return res.json({Spots: userSpots});
+});
+
+router.get("/:spotId", async (req, res, next) => {
+
+
+      const idSpot = await Spot.findByPk (req.params.spotId, {
+
+      });
+
+      if (idSpot) {
+            return res.json(idSpot)
+      } else {
+            res.status(404);
+            return res.json({"message": "Spot couldn't be found"});
+      }
+});
+
+router.get("/", async (req, res, next) => {
+
       const allSpots = await Spot.findAll();
 
       return res.json({Spots: allSpots});
+});
+
+router.post("/:spotId/images", requireAuth, async (req, res, next) => {
+
+      const { url, preview } = req.body;
+
+      const curSpot = await Spot.findByPk(req.params.spotId);
+
+      if (curSpot && curSpot.ownerId === req.user.id) {
+
+            const newSpotImage = await SpotImage.create({
+                  url,
+                  preview,
+                  spotId: curSpot.id
+            });
+
+            return res.json(newSpotImage);
+
+      } else if (curSpot && curSpot.ownerId !== req.user.id) {
+            res.status(403);
+            return res.json({"message": "You don't have the right authorization"});
+      } else {
+            res.status(404);
+            return res.json({"message": "Spot couldn't be found"});
+      }
+});
+
+router.post("/", requireAuth, async (req, res, next) => {
+
+      const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+      const newSpot = await Spot.create({
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price,
+            ownerId: req.user.id
+      });
+
+            res.status(201);
+            return res.json(newSpot);
+
+});
+
+router.put("/:spotId", requireAuth, async (req, res, next) => {
+
+      const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+      const curSpot = await Spot.findByPk(req.params.spotId)
+
+      if (curSpot && curSpot.ownerId === req.user.id) {
+
+            const updatedSpot = await curSpot.update({
+                  address,
+                  city,
+                  state,
+                  country,
+                  lat,
+                  lng,
+                  name,
+                  description,
+                  price
+            });
+
+            return res.json(updatedSpot);
+
+      } else if (curSpot && curSpot.ownerId !== req.user.id) {
+            res.status(403);
+            return res.json({"message": "You don't have the right authorization"});
+      } else {
+            res.status(404);
+            return res.json({"message": "Spot couldn't be found"});
+      }
+
+});
+
+router.delete("/:spotId", requireAuth, async (req, res, next) => {
+
+      const curSpot = await Spot.findByPk(req.params.spotId);
+
+      if (curSpot && curSpot.ownerId === req.user.id) {
+            curSpot.destroy();
+            return res.json({"message": "Successfully deleted"});
+
+      } else if (curSpot && curSpot.ownerId !== req.user.id) {
+            res.status(403);
+            return res.json({"message": "You don't have the right authorization"});
+
+      } else {
+            res.status(404);
+            return res.json({"message": "Spot couldn't be found"});
+      }
 });
 
 module.exports = router;
