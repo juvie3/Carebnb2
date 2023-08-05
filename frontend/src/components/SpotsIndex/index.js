72e21aeff1500091e9e7d88fee93e387f28d13e4
JuvenalBurguillos <juvie3@gmail.com>
2023-08-05 19:18:13 -0400

    added filters but disabled

--- frontend/src/components/SpotsIndex/index.js
+++ frontend/src/components/SpotsIndex/index.js
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchSpots } from "../../store/spotsReducer"
 import { SpotCard } from "../SpotCard"
+import cabin from './cabin.jpeg'
+import beach from './beach.jpeg'
+import tiny from './tiny.jpeg'
+import mansion from './mansions.jpeg'
+import lake from './lake.jpeg'
+import tree from './tree.jpeg'
+import castle from './castle.jpeg'
+import boat from './boats.jpeg'
+import farm from './farms.jpeg'
+import cities from './cities.jpeg'
 
 
 export const SpotsIndex = () => {
@@ -18,17 +28,63 @@ export const SpotsIndex = () => {
 
             return (
                   <>
-                  <div className="filter-holder"   >
-                        <h3 className="filter">Cabins</h3>
-                        <h3 className="filter">Beachfront</h3>
-                        <h3 className="filter">Tiny homes</h3>
-                        <h3 className="filter">Mansions</h3>
-                        <h3 className="filter">Lakefront</h3>
-                        <h3 className="filter">Treehouses</h3>
-                        <h3 className="filter">Castles</h3>
-                        <h3 className="filter">Boats</h3>
-                        <h3 className="filter">Farms</h3>
-                        <h3 className="filter">Iconic cities</h3>
+                  <div className="filter-holder">
+                        <div className='filterBox'>
+                              <img className="filter-icon" src={cabin} />
+                              <div className="filter filter-word">Cabins</div>
+                        </div>
+
+                        <div className='filterBox'>
+                              <img className="filter-icon" src={beach} />
+                              <div className="filter filter-word">Beachfront</div>
+                        </div>
+
+                        <div className='filterBox'>
+                              <img className="filter-icon" src={tiny} />
+                              <div className="filter filter-word">Tiny homes</div>
+                        </div>
+
+                        <div className='filterBox'>
+                              <img className="filter-icon" src={mansion} />
+                              <div className="filter filter-word">Mansions</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={lake} />
+                              <div className="filter filter-word">Lakefront</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={tree} />
+                              <div className="filter filter-word">Treehouses</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={castle} />
+                              <div className="filter filter-word">Castles</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={boat} />
+                              <div className="filter filter-word">Boats</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={farm} />
+                              <div className="filter filter-word">Farms</div>
+
+                        </div>
+                        <div className='filterBox'>
+
+                              <img className="filter-icon" src={cities} />
+                              <div className="filter filter-word">Iconic cities</div>
+
+                        </div>
                   </div>
 
                   <section className="card-holder">
